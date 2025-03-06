4a0af4de2316dadc69c324e32d00ddda866362a3
ricepotato <sukjun40@naver.com>
2025-03-06 12:32:36 +0900

    feat: add post card and enhance home page layout with community discussions

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -1,8 +1,19 @@
+import { Link } from "react-router";
+import { Button } from "~/components/ui/button";
 import { ProductCard } from "~/features/products/components/product-card";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "../components/ui/avatar";
+import { PostCard } from "~/features/community/components/post-card";
 
 export default function HomePage() {
   return (
-    <div className="px-20">
+    <div className="px-20 space-y-40">
       <div className="grid grid-cols-3 gap-4">
         <div>
           <h2 className="text-5xl font-bold leading-tight tracking-tight">
@@ -11,19 +22,39 @@ export default function HomePage() {
           <p className="text-xl font-light text-foreground">
             The best products made by our community today
           </p>
+          <Button variant="link" asChild className="text-lg p-0">
+            <Link to="/products/leaderboards">Explore all products &rarr;</Link>
+          </Button>
         </div>
         <div>
-          {Array.from({ length: 10 }).map((_, index) => (
-            <ProductCard
-              id="productId"
-              name="Product Name"
-              description="Product Description"
-              commentsCount={12}
-              viewsCount={12}
-              upvotesCount={120}
-            />
-          ))}
+          <h2 className="text-5xl font-bold leading-tight tracking-tight">
+            Latest Discussions
+          </h2>
+          <p className="text-xl font-light text-foreground">
+            The latest discussions from our community
+          </p>
+          <Button variant="link" asChild className="text-lg p-0">
+            <Link to="/community">Explore all discussions &rarr;</Link>
+          </Button>
         </div>
+        <PostCard
+          id="postId"
+          title="Discussion Title"
+          author="ricepotato"
+          authorAvatarUrl="https://github.com/ricepotato.png"
+          category="Productivity"
+          postedAt="12 hours ago"
+        />
+        {Array.from({ length: 10 }).map((_, index) => (
+          <ProductCard
+            id="productId"
+            name="Product Name"
+            description="Product Description"
+            commentsCount={12}
+            viewsCount={12}
+            upvotesCount={120}
+          />
+        ))}
       </div>
     </div>
   );
